702bb8959566e20a04ad86ac185f56570a1c2c9d
Bara Abd Al - Rahman <72268356+baranazal@users.noreply.github.com>
2023-01-01 13:03:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import telegram
+import datetime
+import pytz
+from telegram.ext import Updater, CommandHandler
+
+TOKEN = "YOUR_TOKEN"
+
+
+def start(update, context):
+    user = update.message.from_user
+    now = datetime.datetime.now()
+    timezone = pytz.timezone("Asia/Beirut")
+    now = timezone.localize(now)
+    text = "Welcome!!\n\nYou requested your information on {} at {} {}.\n\nHere is the information that you have requested:\n\nHave a great day <3\n\nContact me here: https://wa.me/962791466699".format(now.strftime("%m/%d/%Y"), now.strftime("%I:%M %p"), now.strftime("%z"))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    text = ""
+    if user.first_name:
+        text += "First Name: {}\n".format(user.first_name)
+    if user.last_name:
+        text += "Last Name: {}\n".format(user.last_name)
+    if user.language_code:
+        text += "Language: {}\n".format(user.language_code)
+    if user.id:
+        text += "Chat ID: {}\n".format(user.id)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+updater = Updater(TOKEN, use_context=True)
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+
+updater.start_polling()
+updater.idle()
